1368dbb8cf7d3de43838589d6e95914211e18640
edu9988 <edu_9988@hotmail.com>
2023-05-09 22:15:08 -0300

    Updates Projeto_ConteudoWeb with work done by Adriano

---
+++ Projeto_ConteudoWeb/script.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', reveal);
+window.addEventListener('load', revealTitle);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0; i<reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+function revealTitle(){
+    var revealTitle = document.querySelector('.revealTitle');
+
+    revealTitle.classList.add('active');
+    setTimeout(function revealSubTitle(){
+        var revealSubT = document.querySelector('.revealSubT');
+    
+        revealSubT.classList.add('active');
+    }, 500);
+}
+
